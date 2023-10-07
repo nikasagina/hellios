f866fc421d882db0158e31eaed20579508584b9f
Nikoloz Saginadze <t_nsaginadze@admarketplace.com>
2023-10-08 00:58:52 +0400

    First commit

---
+++ src/main/java/com/example/hellios/HelliosApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelliosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellios/HelliosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
